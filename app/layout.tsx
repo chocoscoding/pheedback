e5b7ec8d8b93867b3ea33a4cbfc4b29d0d25d064
chocoscoding <chocoscoding@gmail.com>
2025-02-02 10:05:22 +0100

    fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,51 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { cn } from "@/lib";
 import { generateMetadata } from "@/utils";
-import { base, heading } from "@/constants";
 import { Toaster } from "@/components/ui/sonner";
-import { subheading } from "@/constants/fonts";
+import { Inter, DM_Sans, Instrument_Serif } from "next/font/google";
+import localFont from "next/font/local";
+
+export const heading = localFont({
+  src: [
+    {
+      path: "./fonts/Satoshi-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Satoshi-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Satoshi-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Satoshi-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Satoshi-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-heading",
+});
+
+export const base = Inter({
+  subsets: ["latin"],
+  variable: "--font-base",
+});
+
+export const subheading = Instrument_Serif({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-subheading",
+});
 
 export const metadata = generateMetadata();
 
